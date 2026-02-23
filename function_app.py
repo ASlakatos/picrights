578dc96ac942025396aa2db9480fa80cb06f519a
ASlakatos <as@lakatoskoves.hu>
2026-02-23 10:52:43 +0100

    Picrights

---
+++ function_app.py
@@ -0,0 +1,15 @@
+import azure.functions as func
+import logging
+import json
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="helloworldlkt")
+def helloworldlkt(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    return func.HttpResponse(
+        json.dumps({"message": "Diego Simeone egy remek ember"}),
+        mimetype="application/json"
+    )
+
